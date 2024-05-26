9c38da799a73ade3e3ee11ebb2d17ab29114f91e
7beunseo <kimes0403@gmail.com>
2024-05-26 16:10:02 +0900

    slug 자동 생성

--- everytime/post/admin.py
+++ everytime/post/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from .models import Post, Category, Comment, PostLike, Scrap
 # Register your models here.
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
 
 admin.site.register(Post)
-admin.site.register(Category)
+# admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(PostLike)
 admin.site.register(Comment)
 admin.site.register(Scrap)
